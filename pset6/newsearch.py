e885036d2b6f0d79470f5c7601167d54c5261c77
Nora J. Singer <NoraJSinger@users.noreply.github.com>
2016-12-19 17:40:46 -0500

    Update newsearch.py

--- pset6/newsearch.py
+++ pset6/newsearch.py
@@ -2,29 +2,39 @@
 """
 Created on Thu Oct 27 12:50:05 2016
 
-@author: Nora Julianna kovacs
+@author: Nora Singer
 """
 
-def main():
+"""Here is code for linear search that uses the fact that a set of elements is sorted in increasing order:"""
+
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        if L[i] > e:
+            return False
+    return False
+ 
     
-    def newsearch(L, e):
-        size = len(L)
-        for i in range(size):
-            if L[size-i-1] == e:
-                return True
-            if L[i] < e:
-                    return False
-        return False
-    
-    list = [2, 3, 7]
-    num = 3
-    
-    if newsearch(list, num):
-        print("true")
-    else:
-        print("false")
-    
-    
-    
-if __name__ == "__main__":
-    main()
+"""Consider the following code, which is an alternative version of search."""
+
+def newsearch(L, e):
+    size = len(L)
+    for i in range(size):
+        if L[size-i-1] == e:
+            return True
+        if L[i] < e:
+            return False
+    return False
+
+
+
+"""Which of the following statements is correct? You may assume that each function 
+is tested with a list L whose elements are sorted in increasing order; for simplicity, 
+assume L is a list of positive integers. 
+
+Correct answer: search and newsearch return the same answers for lists L of length 0, 1, or 2. correct
+
+
+"""
+
