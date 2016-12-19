9bf5de92e77560702d5944ad5119bfa37feee2ab
Nora J. Singer <NoraJSinger@users.noreply.github.com>
2016-12-19 17:06:15 -0500

    Update 2.py

--- pset1/2.py
+++ pset1/2.py
@@ -2,7 +2,13 @@
 """
 Created on Tue Sep 20 15:10:57 2016
 
-@author: Nora Julianna kovacs
+@author: Nora Singer
+
+Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+
+Number of times bob occurs is: 2
 """
 
 # -*- coding: utf-8 -*-
