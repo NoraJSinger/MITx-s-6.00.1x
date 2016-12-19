7b5cbb55952c036a2ca7404f95aa24e43581ab3c
Nora J. Singer <NoraJSinger@users.noreply.github.com>
2016-12-19 17:04:44 -0500

    Update 3.py

--- pset1/3.py
+++ pset1/3.py
@@ -2,7 +2,16 @@
 """
 Created on Tue Sep 20 15:28:52 2016
 
-@author: Nora Julianna kovacs
+@author: Nora Singer
+
+Assume s is a string of lower case characters.
+
+Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
+
+Longest substring in alphabetical order is: beggh
+In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
+
+Longest substring in alphabetical order is: abc
 """
 
 
