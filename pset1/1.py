ce404ce0b4d5772a944df7e573a8879b8135df49
Nora J. Singer <NoraJSinger@users.noreply.github.com>
2016-12-19 17:05:34 -0500

    Update 1.py

--- pset1/1.py
+++ pset1/1.py
@@ -4,6 +4,11 @@ Spyder Editor
 
 This is a temporary script file.
 
+Assume s is a string of lower case characters.
+
+Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
+
+Number of vowels: 5
 
 """
 
